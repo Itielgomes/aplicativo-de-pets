0a1c1c35027e2b5e1f3dcd07f7732ebeae10d4ee
otavio2019 <43707357+otavio2019@users.noreply.github.com>
2024-05-31 17:52:41 -0300

    Update Denuncia.java

--- Denuncia.java
+++ Denuncia.java
@@ -1,17 +1,30 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Denuncia {
     private int idDenuncia;
     private int idAnimal;
-    private int idDenunicante;
+    private int idDenunciante;
     private Date dataDenuncia;
     private String descricao;
+
+    private static List<Denuncia> denunciaList = new ArrayList<>();
+
+
+    public Denuncia(int idDenuncia, int idAnimal, int idDenunciante, Date dataDenuncia, String descricao) {
+        this.idDenuncia = idDenuncia;
+        this.idAnimal = idAnimal;
+        this.idDenunciante = idDenunciante;
+        this.dataDenuncia = dataDenuncia;
+        this.descricao = descricao;
+    }
 
     public int getIdDenuncia() {
         return idDenuncia;
     }
 
-    public void setIdDenuncia(final int idDenuncia) {
+    public void setIdDenuncia(int idDenuncia) {
         this.idDenuncia = idDenuncia;
     }
 
@@ -19,23 +32,23 @@ public class Denuncia {
         return idAnimal;
     }
 
-    public void setIdAnimal(final int idAnimal) {
+    public void setIdAnimal(int idAnimal) {
         this.idAnimal = idAnimal;
     }
 
-    public int getIdDenunicante() {
-        return idDenunicante;
+    public int getIdDenunciante() {
+        return idDenunciante;
     }
 
-    public void setIdDenunicante(final int idDenunicante) {
-        this.idDenunicante = idDenunicante;
+    public void setIdDenunciante(int idDenunciante) {
+        this.idDenunciante = idDenunciante;
     }
 
     public Date getDataDenuncia() {
         return dataDenuncia;
     }
 
-    public void setDataDenuncia(final Date dataDenuncia) {
+    public void setDataDenuncia(Date dataDenuncia) {
         this.dataDenuncia = dataDenuncia;
     }
 
@@ -43,7 +56,17 @@ public class Denuncia {
         return descricao;
     }
 
-    public void setDescricao(final String descricao) {
+    public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
+    public void registrarDenuncia() {
+        denunciaList.add(this);
+        System.out.println("Denúncia registrada com sucesso para o animal ID: " + this.getIdAnimal());
+    }
+
+    public static List<Denuncia> listarDenuncias() {
+        return denunciaList;
+    }
+
 }
