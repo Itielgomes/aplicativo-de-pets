c48dbf15625ed2cebb4a0bd57b449f6339382d5c
RenataMedeir0s <114954319+RenataMedeir0s@users.noreply.github.com>
2024-06-04 02:33:24 -0300

    Doacao.java

--- Doacao.java
+++ Doacao.java
@@ -1,58 +1,78 @@
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+class Doacao extends Doador {
+    private static AtomicInteger idCounter = new AtomicInteger(1);
 
-public class Doacao {
     private int idDoacao;
     private int idPet;
-    private int idDoador;
-    private int idAdotante;
     private Date dataDoacao;
-    private double valor;
+    private double valorDoacao;
+
+    public Doacao(String nome, String email, String senha, String endereco, String telefone, int idPet, Date dataDoacao, double valorDoacao) {
+        super(nome, email, senha, endereco, telefone);
+        this.idDoacao = idCounter.getAndIncrement();
+        this.idPet = idPet;
+        this.dataDoacao = dataDoacao;
+        this.valorDoacao = valorDoacao;
+    }
+ 
+
+    public static AtomicInteger getIdCounter() {
+        return idCounter;
+    }
+
+
+    public static void setIdCounter(AtomicInteger idCounter) {
+        Doacao.idCounter = idCounter;
+    }
+
 
     public int getIdDoacao() {
         return idDoacao;
     }
 
-    public void setIdDoacao(final int idDoacao) {
+
+    public void setIdDoacao(int idDoacao) {
         this.idDoacao = idDoacao;
     }
+
 
     public int getIdPet() {
         return idPet;
     }
 
-    public void setIdPet(final int idPet) {
+
+    public void setIdPet(int idPet) {
         this.idPet = idPet;
     }
 
-    public int getIdDoador() {
-        return idDoador;
-    }
-
-    public void setIdDoador(final int idDoador) {
-        this.idDoador = idDoador;
-    }
-
-    public int getIdAdotante() {
-        return idAdotante;
-    }
-
-    public void setIdAdotante(final int idAdotante) {
-        this.idAdotante = idAdotante;
-    }
 
     public Date getDataDoacao() {
         return dataDoacao;
     }
 
-    public void setDataDoacao(final Date dataDoacao) {
+
+    public void setDataDoacao(Date dataDoacao) {
         this.dataDoacao = dataDoacao;
     }
 
-    public double getValor() {
-        return valor;
+
+    public double getValorDoacao() {
+        return valorDoacao;
     }
 
-    public void setValor(final double valor) {
-        this.valor = valor;
+
+    public void setValorDoacao(double valorDoacao) {
+        this.valorDoacao = valorDoacao;
     }
+       
+
+    public void registrarDoacao() {
+        System.out.println("Doação registrada com sucesso. ID da doação: " + this.idDoacao);
+    }
+
+    public void processarDoacao() {
+        System.out.println("Doação processada com sucesso.");
+    }
+
 }
