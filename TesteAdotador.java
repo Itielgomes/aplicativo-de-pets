d7b98ca3453acc28dd181eb1e1ca42179852335f
Itielgomes <andregomes1@ads.fiponline.edu.br>
2024-06-05 17:54:45 -0300

    Update TesteAdotador.java

--- TesteAdotador.java
+++ TesteAdotador.java
@@ -5,10 +5,10 @@ class AdotadorTest {
 
     @Test
     public void testGetIdAdotador() {
-        Adotador adotador = new Adotador("João", "joao@example.com", "senha123", "Rua A, 123", "123456789");
+        Adotador adotador = new Adotador("André", "andre@example.com", "senha123", "Rua A, 123", "123456789");
         assertEquals(1, adotador.getIdAdotador());
 
-        Adotador outroAdotador = new Adotador("Maria", "maria@example.com", "senha456", "Rua B, 456", "987654321");
+        Adotador outroAdotador = new Adotador("Otávio", "otavio@example.com", "senha456", "Rua B, 456", "987654321");
         assertEquals(2, outroAdotador.getIdAdotador());
     }
 }
